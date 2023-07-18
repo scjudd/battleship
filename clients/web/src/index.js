42aad06521f2c5dec0c695965346e03e6953a076
Spencer Judd <spencercjudd@gmail.com>
2023-07-18 11:00:51 -0400

    Start a new game on app load

--- clients/web/src/index.js
+++ clients/web/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {NewGameRequest} from './proto/battleship_pb.js';
+import {BattleshipClient} from './proto/battleship_grpc_web_pb.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +12,27 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+function newGame() {
+	console.log("starting a new game");
+
+	let client = new BattleshipClient("http://localhost:8080")
+	let request = new NewGameRequest();
+	client.newGame(request, {}, function(err, response) {
+		if (err) {
+			console.log(err);
+			return
+		}
+
+		window.gameID = response.getGameid();
+		window.playerID = response.getPlayerid();
+
+		console.log("Game ID", window.gameID);
+		console.log("Player ID", window.playerID);
+	});
+}
+
+newGame();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
